0ff5d4c1cd344ffae486c2262cac93d11aa02001
kirti gaurav mishra <kirtigauravmishra@gmail.com>
2022-06-26 22:32:56 +0530

    categories added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
+import {AiOutlineCheck} from 'react-icons/ai';
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
@@ -38,6 +39,11 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
           onClick={() => setEdit({ id: todo.id, value: todo.text })}
           className='edit-icon'
         />
+        <
+          AiOutlineCheck
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+          />
       </div>
     </div>
   ));
---
+++ src/components/category.js
@@ -0,0 +1,14 @@
+const incomeColors = ['#123123', '#154731', '#165f40', '#16784f', '#14915f', '#10ac6e', '#0bc77e', '#04e38d', '#00ff9d'];
+
+
+export const categorytodo = [
+  { type: 'Business', color: incomeColors[0] },
+  { type: 'Cooking',  color: incomeColors[1] },
+  { type: 'Studying', color: incomeColors[2] },
+  { type: 'Working',  color: incomeColors[3] },
+  { type: 'Playing',  color: incomeColors[4] },
+  { type: 'singing',  color: incomeColors[5] },
+  { type: 'Sleeping',  color: incomeColors[6] },
+  { type: 'Enjoying', color: incomeColors[7] },
+  { type: 'Others', color: incomeColors[8] },
+];
